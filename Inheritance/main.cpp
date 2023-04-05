cc231d6a55163dd2c148e310131651d7ae2900ce
Koval_nc30@top-academy.ru <Koval_nc30@top-academy.ru>
2023-04-05 14:23:33 +0300

    Add project files.

---
+++ Inheritance/main.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+class Animal
+{
+	virtual void sound() = 0;
+};
+class Cat : public Animal
+{
+public:
+	void sound()
+	{
+		cout << "Meow" << endl;
+	}
+};
+class Dog : public Animal
+{
+public:
+	void sound()
+	{
+		cout << "Woof" << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Cat tom;
+	tom.sound();
+
+	Dog hatico;
+	hatico.sound();
+}
